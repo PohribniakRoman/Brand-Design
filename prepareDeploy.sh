41cc0d035860f0705d0d9ba6733dab2af8314486
Roman <roman.pohribniak@gmail.com>
2021-03-05 00:01:01 +0200

    build script

--- prepareDeploy.sh
+++ prepareDeploy.sh
@@ -4,9 +4,21 @@ rm -rf dist/
 
 echo "Installing packages"
 
-# npm i
+npm i
 npm run build
 
 rm -rf src/
 
 mv dist/* .
+
+git branch -D gh-pages 
+
+git checkout -b gh-pages
+
+git add -A
+
+git commit -m "deploy"
+
+git push -f origin gh-pages
+
+git checkout main
