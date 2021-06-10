9bf4d62bb4d21683b8c7091b93f3c84fafe5d7bd
Roman <roman.pohribniak@gmail.com>
2021-06-10 18:31:53 +0300

    slider

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,31 +1,29 @@
 const controls = {
-    left:document.querySelector(".slider__control--left"),
-    right:document.querySelector(".slider__control--right"),
-    index:0
-}
-const slider = [...document.querySelectorAll(".slider__container")];
+  left: document.querySelector(".slider__control--left"),
+  right: document.querySelector(".slider__control--right"),
+};
+let index = 0;
 
+const len = document.querySelectorAll(".slider__container").length;
+const slider = document.querySelector(".slider__container");
+slider.style.marginLeft = "0%";
 //logic
 
-controls.left.addEventListener("click",()=>{
-    if(controls.index === 0){
-        slider[controls.index].classList.add("hidden");
-        slider[slider.length -1].classList.remove("hidden");
-        controls.index = slider.length -1;
-    }else{
-        slider[controls.index].classList.add("hidden");
-        slider[controls.index -1].classList.remove("hidden");
-        controls.index--;
-    }
-})
-controls.right.addEventListener("click",()=>{
-    if(controls.index === slider.length - 1){
-        slider[controls.index].classList.add("hidden");
-        slider[0].classList.remove("hidden");
-        controls.index = 0;
-    }else{
-        slider[controls.index].classList.add("hidden");
-        slider[controls.index +1].classList.remove("hidden");
-        controls.index++;
-    }
-})
+controls.left.addEventListener("click", () => {
+  if (index === 0) {
+    index = len-1;
+    slider.style.marginLeft = `-${(index) * 100}%`;
+  } else {
+    index--;
+    slider.style.marginLeft = `-${index * 100}%`;
+  }
+});
+controls.right.addEventListener("click", () => {
+  if (index === len - 1) {
+    slider.style.marginLeft = "0";
+    index = 0;
+  } else {
+    index++;
+    slider.style.marginLeft = `-${index * 100}%`;
+  }
+});
